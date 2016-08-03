6dad160f0cb40abd74941af2c40d5ef59ebae417
Richard Adem <richy486@gmail.com>
2016-08-03 13:23:39 -0400

    Hooked up video player

--- videoPlayer/ViewController.swift
+++ videoPlayer/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, VideoViewDataSource, VideoViewDelegate {
 
+    @IBOutlet weak var videoView: VideoView? {
+        didSet {
+            if let videoView = videoView{
+                videoView.dataSource = self
+                videoView.delegate = self
+            }
+        }
+    }
+    
+    // MARK: View controller lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        videoView?.play()
+    }
 
+    // MARK: Video view data source
+    func videoViewUrlString(_ videoView: VideoView) -> String? {
+        return "video.mp4"
+    }
+    
+    // MARK: Video view delegate
+    
+    func videoView(_ videoView:VideoView, timeDidChange time:CMTime) {
+    }
+    func videoViewVideoDidEnd(_ videoView:VideoView) {
+    }
+    
+    // MARK: Memory management
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
